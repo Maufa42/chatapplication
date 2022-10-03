2f8302b59e367a430aa605aba070891641fe04b9
Aryan Pal <aryanpalongraph@gmail.com>
2022-10-03 14:03:41 +0530

    Updated name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'room/index'
+  resources :rooms
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
