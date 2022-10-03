9407191eec5913aab853b3c24fb349c58ea04070
Aryan Pal <aryanpalongraph@gmail.com>
2022-10-03 13:58:04 +0530

    ➕ Added Rooms Controoler

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,11 @@
+class RoomController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def index
+    @rooms = Room.public_rooms;
+    @users = User.all_except(current_user);
+  end
+
+
+end
